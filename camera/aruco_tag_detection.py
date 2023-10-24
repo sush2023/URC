72424c1304e3ff6adb9bebb537b5b497fa52747e
Matt Bartie <mattbartie3@gmail.com>
2023-10-24 20:01:42 +0000

    just cleaned up code/comments

--- camera/aruco_tag_detection.py
+++ camera/aruco_tag_detection.py
@@ -3,17 +3,17 @@ import cv2.aruco as aruco
 
 parameters = aruco.DetectorParameters_create()
 
-# Load the dictionary, 4x4_50 is what we used according to rule book
-aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+# Load the dictionary, 4x4_50 is what we use according to rule book
+arucoDict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 
-# cv2.CAP_V4L was needed to run on my machine, might need to change for rover
-cap = cv2.VideoCapture(0, cv2.CAP_V4L)
+# cv2.CAP_V4L says to use Video4linux2 driver, might be different for jetson
+capObj = cv2.VideoCapture(0, cv2.CAP_V4L)
 
 while True:
-    ret, frame = cap.read()
+    returnedValue, frame = capObj.read()
 
     # Detects markers in the image
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
+    corners, ids, rejectedTags = aruco.detectMarkers(frame, arucoDict, parameters=parameters)
 
     # If markers detected
     if len(corners) > 0:
@@ -24,6 +24,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+capObj.release()
 cv2.destroyAllWindows()
 
